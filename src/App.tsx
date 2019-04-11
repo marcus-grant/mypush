6782f678bc27820470c70fcaaf9c8c47e72683ca
Marcus Grant <marcusfg@gmail.com>
2019-04-10 23:59:25 -0700

    partial commit of setting the responsive app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
 import * as React from 'react';
 import './App.css';
 
@@ -6,17 +10,36 @@ import logo from './logo.svg';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        <Button variant="contained" color="primary">Hello</Button>
+        </div>
+        <AppBar
+          position="fixed"
+          color="primary"
+          className={classes.appBar}
+        />
+      </React.Fragment>
     );
   }
 }
+
+const BottomAppBar = props => {
+  const { classes } = props;
+  return (
+    <AppBar position="fixed" color="primary" className={classes.appBar}>
+      <Toolbar className={classes.toolbar} >
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default App;
